7f2e140dd1a850b23f2c5a15bb8b66753196a919
quinque5 <76754852+djg9@users.noreply.github.com>
2021-06-28 10:56:54 -0400

    Update i and j, add function call

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,9 @@ let quotes = ['To truly find yourself, you should play hide and seek alone', 'Er
 let fortunes = ['You will reconnect with an old friend', 'You will pay for a meal', 'An unexpected twist of fate lies ahead', 'Smooth sailing ahead', 'You will fall asleep tonight'];
 
 let generateMessage = () => {
-    let i = Math.floor(Math.random() * 5);
-    let j = Math.floor(Math.random() * 6);
+    let i = Math.floor(Math.random() * 4);
+    let j = Math.floor(Math.random() * 5);
     console.log(`${quotes[i]}. The cookie says: ${fortunes[j]}. Your lucky number is ${Math.floor(Math.random() * 100)}.`);
 };
 
+generateMessage();
