e7e9b31f9642d5d5c9d598526634ebe47b826ad7
quinque5 <76754852+djg9@users.noreply.github.com>
2021-06-28 10:43:51 -0400

    Add quotes and fortunes

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+let quotes = ['To truly find yourself, you should play hide and seek alone', 'Error 404: Quote not found', 'Do or do not, there is no try', 'Avoid people who take fortune cookies seriously'];
+let fortunes = ['You will reconnect with an old friend', 'You will pay for a meal', 'An unexpected twist of fate lies ahead', 'Smooth sailing ahead', 'You will fall asleep tonight'];
+
