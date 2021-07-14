1d63921fd458817ff5d4d2e0620288696d0d422e
quinque5 <76754852+djg9@users.noreply.github.com>
2021-07-14 16:12:57 -0500

    Update quotes and fortunes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-let quotes = ['To truly find yourself, you should play hide and seek alone', 'Error 404: Quote not found', 'Do or do not, there is no try', 'Avoid people who take fortune cookies seriously'];
-let fortunes = ['You will reconnect with an old friend', 'You will eat some food', 'An unexpected twist of fate lies ahead', 'Smooth sailing ahead', 'You will sleep tonight'];
+let quotes = ['To truly find yourself, you should play hide and seek alone', 'Error 404: Advice not found', 'Do or do not, there is no try', 'Avoid people who take fortune cookies seriously'];
+let fortunes = ['You will reconnect with an old acquaintance', 'You will eat some food', 'An unexpected twist of fate lies ahead', 'Smooth sailing ahead', 'You will sleep tonight'];
 
 let genButton = document.getElementById('gen');
 let messageDisplay = document.getElementById('message');
@@ -9,7 +9,4 @@ genButton.addEventListener('click', () => {
     let j = Math.floor(Math.random() * 5);
     messageDisplay.innerText = `${quotes[i]}. The cookie says: ${fortunes[j]}. Your lucky number is ${Math.floor(Math.random() * 100)}.`;
     document.querySelector('.button').disabled = false;
-
 });
-
-// commit changes
