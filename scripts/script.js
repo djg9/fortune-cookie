9770e9fb88d407e5cf60235094e70cb907ccec0e
quinque5 <76754852+djg9@users.noreply.github.com>
2021-07-14 11:24:10 -0500

    Significant changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 let quotes = ['To truly find yourself, you should play hide and seek alone', 'Error 404: Quote not found', 'Do or do not, there is no try', 'Avoid people who take fortune cookies seriously'];
-let fortunes = ['You will reconnect with an old friend', 'You will pay for a meal', 'An unexpected twist of fate lies ahead', 'Smooth sailing ahead', 'You will fall asleep tonight'];
+let fortunes = ['You will reconnect with an old friend', 'You will eat some food', 'An unexpected twist of fate lies ahead', 'Smooth sailing ahead', 'You will sleep tonight'];
 
-let generateMessage = () => {
+let genButton = document.getElementById('gen');
+let messageDisplay = document.getElementById('message');
+
+genButton.addEventListener('click', () => {
     let i = Math.floor(Math.random() * 4);
     let j = Math.floor(Math.random() * 5);
-    console.log(`${quotes[i]}. The cookie says: ${fortunes[j]}. Your lucky number is ${Math.floor(Math.random() * 100)}.`);
-};
+    messageDisplay.innerText = `${quotes[i]}. The cookie says: ${fortunes[j]}. Your lucky number is ${Math.floor(Math.random() * 100)}.`;
+    document.querySelector('.button').disabled = false;
 
-generateMessage();
+});
+
+// commit changes
